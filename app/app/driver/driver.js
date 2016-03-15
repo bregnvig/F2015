e8b77cb5c131076659aab24701a81314c82118a2
Flemming Bregnvig <flemming@bregnvig.dk>
2016-03-15 20:07:38 +0100

    Moved more to components

--- app/app/driver/driver.js
+++ app/app/driver/driver.js
@@ -5,15 +5,9 @@ angular.module('f2015.driver', ['f2015.model.driver', 'f2015.model.ergast'])
     $stateProvider
       .state('f2015.drivers', {
         url: '/drivers',
-        resolve: {
-          allDrivers: ['driverModel', function(driverModel) {
-            return driverModel.all;
-          }]
-        },
         views: {
           '@': {
-            templateUrl: 'app/driver/drivers.tmpl.html',
-            controller: 'DriversCtrl as drivers'
+            template: '<drivers></drivers>'
           }
         }
       })
@@ -21,76 +15,65 @@ angular.module('f2015.driver', ['f2015.model.driver', 'f2015.model.ergast'])
         url: '/{id}',
         views: {
           '@': {
-            templateUrl: 'app/driver/driver.tmpl.html',
-            controller: 'DriverCtrl as driver'
+            template: '<driver></driver>'
           }
         }
       });
   }])
-  .controller('DriversCtrl', ['$state', 'allDrivers', 'driverModel', 'ergastModel', function($state, allDrivers, driverModel, ergastModel) {
-    var drivers = this;
-    allDrivers.$promise.then(function() {
-      drivers.all = allDrivers;
-      ergastModel.getStandings(function(standings) {
-        standings.forEach(function(standing) {
-          drivers.all.forEach(function(driver) {
-            if (driver.code === standing.Driver.driverId) {
-              driver.points = standing.points;
-            }
+  .component('drivers', {
+    templateUrl: 'app/driver/drivers.tmpl.html',
+    controller: ['$state', 'driverModel', 'ergastModel', function($state, driverModel, ergastModel) {
+      const $ctrl = this;
+      driverModel.all.$promise.then((all) => {
+        $ctrl.all = all;
+        ergastModel.getStandings((standings) => {
+          standings.forEach((standing) => {
+            $ctrl.all.forEach((driver) => {
+              if (driver.code === standing.Driver.driverId) {
+                driver.points = standing.points;
+              }
+            });
           });
-        });
-        drivers.all.sort(function(a, b) {
-          return b.points - a.points;
+          $ctrl.all.sort((a, b) => b.points - a.points);
         });
       });
-    });
-    drivers.navigateTo = function(state, params) {
-      $state.go(state, params);
-    };
-  }])
-  .controller('DriverCtrl', ['$scope', '$stateParams', 'ergastModel', 'allDrivers', 'credentials', function($scope, $stateParams, ergastModel, allDrivers, credentailsProvider) {
-    var vm = this;
-    credentailsProvider().then(function(credentials) {
-      vm.credentials = credentials;
-    });
-    vm.previousYear = ergastModel.previousSeason;
-    vm.currentYear = ergastModel.currentSeason;
-    allDrivers.$promise.then(function() {
-      var filtered = allDrivers.filter(function(temp) {
-        return temp.id === parseInt($stateParams.id);
+      $ctrl.navigateTo = (state, params) => $state.go(state, params);
+    }]
+  })
+  .component('driver', {
+    templateUrl: 'app/driver/driver.tmpl.html',
+    controller: ['$scope', '$stateParams', 'ergastModel', 'driverModel', 'credentials', function($scope, $stateParams, ergastModel, driverModel, credentailsProvider) {
+      const $ctrl = this;
+      $ctrl.previousYear = ergastModel.previousSeason;
+      $ctrl.currentYear = ergastModel.currentSeason;
+
+      credentailsProvider().then((credentials) => $ctrl.credentials = credentials);
+      driverModel.all.$promise.then((all) => {
+        $ctrl.driver = all.find((temp) => temp.id === parseInt($stateParams.id));
+        if ($ctrl.driver) {
+          $ctrl.qualifyResult = ergastModel.getLastYearDriverQualify($ctrl.driver.code);
+          $ctrl.raceResult = ergastModel.getLastYearDriverResults($ctrl.driver.code);
+          ergastModel.getCurrentYearDriverStatus($ctrl.driver.code, (result) => {
+            $ctrl.retired = result.reduce((previousValue, status) => previousValue + (status.status === 'Finished' ? 0 : parseInt(status.count)), 0).toFixed(1);
+          });
+          ergastModel.getCurrentYearDriverQualify($ctrl.driver.code, (result) => {
+            $ctrl.currentQualifyResult = result;
+            var grid = result.reduce((previousValue, race) => previousValue + parseInt(race.QualifyingResults[0].position), 0);
+            $ctrl.avgGrid = result.length !== 0 ? (grid / result.length).toFixed(1) : '-';
+          });
+          ergastModel.getCurrentYearDriverResults($ctrl.driver.code, (result) => {
+            $ctrl.currentRaceResult = result;
+            var position = result.reduce((previousValue, race) => previousValue + parseInt(race.Results[0].position), 0);
+            $ctrl.avgPosition = result.length !== 0 ? (position / result.length).toFixed(1) : '-';
+          });
+        }
       });
-      console.log('Driver', vm.get);
-      if (filtered && filtered.length) {
-        vm.get = filtered[0];
-        vm.qualifyResult = ergastModel.getLastYearDriverQualify(vm.get.code);
-        vm.raceResult = ergastModel.getLastYearDriverResults(vm.get.code);
-        ergastModel.getCurrentYearDriverStatus(vm.get.code, function(result) {
-          vm.retired = result.reduce(function(previousValue, status) {
-            return previousValue + (status.status === 'Finished' ? 0 : parseInt(status.count));
-          }, 0).toFixed(1);
-        });
-        ergastModel.getCurrentYearDriverQualify(vm.get.code, function(result) {
-          vm.currentQualifyResult = result;
-          var grid = result.reduce(function(previousValue, race) {
-            return previousValue + parseInt(race.QualifyingResults[0].position);
-          }, 0);
-          vm.avgGrid = result.length !== 0 ? (grid / result.length).toFixed(1) : '-';
-        });
-        ergastModel.getCurrentYearDriverResults(vm.get.code, function(result) {
-          vm.currentRaceResult = result;
-          var position = result.reduce(function(previousValue, race) {
-            return previousValue + parseInt(race.Results[0].position);
-          }, 0);
-          vm.avgPosition = result.length !== 0 ? (position / result.length).toFixed(1) : '-';
-        });
-      }
-    });
-    $scope.$watch(function() {
-      return vm.get ? vm.get.active : undefined;
-    }, function(newValue) {
-      if (newValue !== undefined) {
-        vm.get.partOfSeason = true;
-        vm.get.$save();
-      }
-    });
-  }]);
+      $scope.$watch(() => $ctrl.driver ? $ctrl.driver.active : undefined, (newValue) => {
+        if (newValue !== undefined) {
+          $ctrl.driver.partOfSeason = true;
+          $ctrl.driver.$save();
+        }
+      });
+    }]
+  });
+
